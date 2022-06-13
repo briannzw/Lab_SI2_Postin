6de59d585e7b40991e188c3adccb73733f9f338a
briannzw <brianwijaya2003@gmail.com>
2022-06-13 17:56:14 +0700

    Update Database

--- post_in.sql
+++ post_in.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Jun 2022 pada 19.06
+-- Waktu pembuatan: 13 Jun 2022 pada 12.55
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 8.0.3
 
@@ -40,11 +40,13 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`id`, `user`, `image`, `caption`, `created_at`) VALUES
-('1', 'user_01', 'http://picsum.photos/200/200', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut e', '2022-06-12 16:41:13'),
-('62a61255d5f929.21504992', '', '', 'tes', '2022-06-12 23:20:37'),
+('62a61255d5f929.21504992', 'aa', 'img_62a716babbd1c738856530.jpg', 'tes', '2022-06-12 23:20:37'),
 ('62a61326dff211.23229821', 'admin', '', 'I\'m admin', '2022-06-12 23:24:06'),
 ('62a61811b3c976.38269861', 'admin', '', 'alo', '2022-06-12 23:45:05'),
-('62a61ab054a0e7.81672608', 'admin', '', 'test123', '2022-06-12 23:56:16');
+('62a61ab054a0e7.81672608', 'admin', '', 'test123', '2022-06-12 23:56:16'),
+('62a70083c8ac81.48545243', 'admin', '', 'test', '2022-06-13 16:16:51'),
+('62a701da1ffff5.83665195', 'admin', 'img_62a7159048036553847673.png', 'test', '2022-06-13 16:22:34'),
+('62a701fb5eede8.23220837', 'admin', 'img_62a71411164fa584854130.jpg', 'test', '2022-06-13 16:23:07');
 
 -- --------------------------------------------------------
 
@@ -69,9 +71,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `username`, `password`, `avatar`, `admin`, `created_at`, `last_login`) VALUES
-('6118b2a943acc2.78631959', 'Administrator', 'admin@mail.com', 'admin', '$2y$10$KNFkU8Uyj/PPKm4GOs0nj.E.eXiof3Wjhku8wBGL6yZs4dOerpsEO', NULL, 1, '2021-08-14 23:22:33', '2022-06-12 11:56:04'),
-('62a5e8b1621f44.48183918', 'aa', 'aaa@aa.com', 'aaaa', '$2y$10$gCzYT2urBBwJ7Bom44/.juctWMAlYoiXCtCUxzOGkkMd6wVTx.YAC', NULL, 0, '2022-06-12 13:22:57', '2022-06-12 08:23:54'),
-('62a61ad13669d8.07598377', 'aa', 'aa@aa.com', 'aa', '$2y$10$B2YA0JjNDihM4D3piKQGuegem.ujWgwqID0uvulxQsqXvT/3UEQF2', NULL, 0, '2022-06-12 16:56:49', '2022-06-12 11:56:51');
+('6118b2a943acc2.78631959', 'Administrator', 'admin@mail.com', 'admin', '$2y$10$KNFkU8Uyj/PPKm4GOs0nj.E.eXiof3Wjhku8wBGL6yZs4dOerpsEO', '6118b2a943acc278631959.png', 1, '2021-08-14 23:22:33', '2022-06-13 05:48:42'),
+('62a5e8b1621f44.48183918', 'aa', 'aaa@aa.com', 'aaaa', '$2y$10$gCzYT2urBBwJ7Bom44/.juctWMAlYoiXCtCUxzOGkkMd6wVTx.YAC', '', 0, '2022-06-12 13:22:57', '2022-06-12 08:23:54'),
+('62a6ef1de5fc78.16513731', 'aa', 'aa@aa.com', 'aa', '$2y$10$7pAGpau0wUvfTTQKEyvULuxHQ7V6TL/6pXZErCNvR/bTkiIpY2hQq', '', 0, '2022-06-13 08:02:37', '2022-06-13 03:02:40');
 
 --
 -- Indexes for dumped tables
